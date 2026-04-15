9732f7f616b6ec7aa476a80046ce202eca93e9a9
Tom De Nies <tom.denies@gmail.com>
2026-04-15 18:57:56 +0200

    Coupled actual backend service to question-answering service.

--- app/services/question-answering.ts
+++ app/services/question-answering.ts
@@ -1,8 +1,9 @@
 import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import fetch from 'fetch';
 
 export interface Source {
-  href: string;
+  uri: string;
   title: string;
   approved?: boolean;
   rejected?: boolean;
@@ -22,26 +23,38 @@ export default class QuestionAnsweringService extends Service {
   @tracked answer?: Answer;
 
   async sendQuestion() {
-    // TODO: this needs to be connected to the actual backend instead of this mockup
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    let content = 'The 17 Sustainable Development Goals (SDGs) are a set of global goals adopted by all United Nations member states in 2015 as part of the 2030 Agenda for Sustainable Development. They aim to improve life for people and protect the planet. Here are all 17 goals: No Poverty, Zero Hunger, Good Health and Well-being, Quality Education, Gender Equality, Clean Water and Sanitation, Affordable and Clean Energy, Decent Work and Economic Growth, Industry, Innovation and Infrastructure, Reduced Inequalities, Sustainable Cities and Communities, Responsible Consumption and Production, Climate Action, Life Below Water, Life on Land, Peace, Justice and Strong Institutions, Partnerships for the Goals.';
-    let sources = [
+    const resp = await fetch(
+      "/question-answering/answer",
       {
-        href: 'https://www.un.org/sustainabledevelopment/development-goals/?utm_source=chatgpt.com',
-        title: 'UN Sustainable Development Goals overview',
-      },
-      {
-        href: 'https://sdgs.un.org/goals?utm_source=chatgpt.com',
-        title: 'Full list of all 17 SDGs (sdgs.un.org)'
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          "question": this.currentQuestion,
+          "top_n": 3
+        }),
       }
-    ];
-    // TODO: remove this test for unasnwerable questions
-    if (this.currentQuestion.indexOf('impossible question') > -1) {
-      content = 'Sorry, we were unable to find any relevant information in any decision. Try rewording your question or giving more details, e.g. which local authority you’re asking the question about.';
-      sources = [];
+    );
+    let content: string = "";
+    let sources = [];
+    let id = '0';
+    if (!resp.ok) {
+      content = "Unfortunately, our service encountered an error. Please try again later."
+    } else {
+      const payload = await resp.json();
+      if (payload?.answer) {
+        content = payload.answer;
+      }
+      if (payload?.sources) {
+        sources = payload.sources;
+      }
+      if (payload?.id) {
+        id = payload.id;
+      }
     }
     this.answer = {
-      id: '12345',
+      id,
       content,
       time: new Date(),
       sources
